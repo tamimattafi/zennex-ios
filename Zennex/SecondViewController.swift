4ba8d796ba8c65745c178ad80bea711f0bbe4335
Tamim Attafi <tamim.tanki@gmail.com>
2020-03-16 23:29:00 +0700

    Initial Commit

---
+++ Zennex/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Zennex
+//
+//  Created by Zennex on 16/03/2020.
+//  Copyright © 2020 tamimattafi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
